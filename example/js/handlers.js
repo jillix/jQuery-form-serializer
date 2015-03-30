8d1fd438d3119701fd1ab78c73504ffbc5750e5a
Ionică Bizău <bizauionica@yahoo.com>
2015-03-30 18:16:34 +0300

    Fill form example

--- example/js/handlers.js
+++ example/js/handlers.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     $("form")
         .serializer()
         .on("serializer:data", function (e, data) {
+            if (e.target.id === "fill-form") {
+                return $(".demo-form").trigger("serializer:fill", [data.content]);
+            }
             alert(JSON.stringify(data, null, 4));
         })
         ;
